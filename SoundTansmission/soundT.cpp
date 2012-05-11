220e21d8390c9d2955218c4773490bda9f615fc7
ilnur-slv <bilnurbk@ya.ru>
2012-05-11 23:59:25 +0600

    [+] Добавил Сервер и Клиентскую часть
    (-) Он почему то не работает

--- SoundTansmission/soundT.cpp
+++ SoundTansmission/soundT.cpp
@@ -5,12 +5,16 @@
 #include <MMSystem.h>
 #include <string.h>
 #include <fstream>
+#include <WS2tcpip.h>
+#include "winsock2.h"
 #pragma comment( lib, "winmm" ) //--подключаем winmm.dll--
+#pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
 
 #define IDC_B1 (1234) //--кнопка воспроизведения--
 #define IDC_B2 (1235) //--кнопка выхода--
+#define IDC_B3 (1237) //--кнопка начала передачи--
 #define IDC_E1 (1236) //--текстовое поле для вывода ошибок--
 
 LRESULT CALLBACK MyFunc(HWND, UINT, WPARAM, LPARAM);
@@ -28,11 +32,16 @@ char *sound_buffer; //--аудиопоток--
 int size; //--размер аудиопотока--
 const int sizeWaveHdr = 8; //--количество аудиопотоков--
 WAVEHDR waveHdr[sizeWaveHdr]; //--массив для аудиопотоков--
-int bN = 0;
+bool state[sizeWaveHdr]; //--показывает буфер полон или нет--
+int bN = 0; //--номер следующего по списку свободного буфера--
+int emptyBuffer = 0; //--номер очищаемого буфера--
 
 //--Объявления функциий--------------------------------------------------------------------------------------
 
+DWORD WINAPI ServerThread(LPVOID pParam);
+DWORD WINAPI ClientThread(LPVOID pParam);
 void PrintText(string text);   //--печать ошибок--
+void PrintText(int number);    //--печать ошибок--
 int ParseData();               //--создание аудиопотока--
 void PlayBuffer(char *Buffer); //--воспроизведение буфера--
 void PlaySound();              //--воспроизведение--
@@ -44,9 +53,9 @@ int WINAPI WinMain(HINSTANCE hThisInst, HINSTANCE hPrevInst, LPSTR lpszArgs, int
 	MSG msg;
 	WNDCLASS wcl;
 
-	HWND hButton1, hButton2;
+	HWND hButton1, hButton2, hButton3;
 
-	wcl.hInstance = NULL; // hThisInst;
+	wcl.hInstance = NULL;// hThisInst;
 	wcl.lpszClassName = szWinName;
 	wcl.lpfnWndProc = MyFunc;
 	wcl.style = 0;
@@ -72,6 +81,10 @@ int WINAPI WinMain(HINSTANCE hThisInst, HINSTANCE hPrevInst, LPSTR lpszArgs, int
 	CreateWindowA(
 		"BUTTON", "Exit", WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_DEFPUSHBUTTON,
 		300, 400, 100, 24, hWnd, (HMENU)IDC_B2, NULL, NULL);
+
+	CreateWindowA(
+		"BUTTON", "Start", WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_DEFPUSHBUTTON,
+		100, 370, 100, 24, hWnd, (HMENU)IDC_B3, NULL, NULL);
 
 	hEdit = CreateWindowA(
 		"EDIT","",WS_CHILD|WS_VISIBLE|WS_BORDER|WS_VSCROLL|WS_HSCROLL|ES_MULTILINE,
@@ -106,15 +119,12 @@ LRESULT CALLBACK MyFunc(HWND this_hwnd,UINT message, WPARAM wParam, LPARAM lPara
 		{
 			//--Очищаем память-------------------------------------------------------------------------------
 
-			for(int i=0; i<sizeWaveHdr; ++i)
-			{
-				if(waveHdr[i].dwLoops == 1)
-				{
-					waveOutUnprepareHeader(hWaveOut, &waveHdr[i], sizeof(waveHdr));
-					free(waveHdr[i].lpData);
-				}
-			}
-			//waveOutClose(hWaveOut);
+			/*waveOutUnprepareHeader(hWaveOut, &waveHdr[emptyBuffer], sizeof(waveHdr));
+			free(waveHdr[emptyBuffer].lpData);*/
+
+			state[emptyBuffer] = false;
+
+			emptyBuffer = (emptyBuffer == 7) ? 0 : emptyBuffer + 1;
 
 			//--Действие если аудиопоток закончился----------------------------------------------------------
 
@@ -129,11 +139,19 @@ LRESULT CALLBACK MyFunc(HWND this_hwnd,UINT message, WPARAM wParam, LPARAM lPara
 			if(idButton == IDC_B1)
 			{
 				PlaySound();
+				//--Нужно добавить создание потока--
+				//--Этот поток должен работать так же как и PlaySound()--
 			}
 
 			if(idButton == IDC_B2)
 			{
 				SendMessage(hWnd, WM_DESTROY, 0, 0);
+			}
+
+			if(idButton == IDC_B3)
+			{
+				//--Нужно добавить создание потока--
+				//--Поток должен каждый раз обращатся к функции PlayBuffer()--
 			}
 
 			break;
@@ -257,19 +275,37 @@ void PlayBuffer(char *Buffer){
 	//file << *(Buffer + i);
 	//file.close();
 
-	//--Подготовка буфера для записи-------------------------------------------------------------------------
+	//state[bN] = true;
 
-	ZeroMemory(&waveHdr[bN], sizeof(waveHdr[bN])); //--обнуляем WaveHdr--
+	////--Подготовка буфера для записи-------------------------------------------------------------------------
 
-	waveHdr[bN].lpData = Buffer; //--присваиваем аудиопоток для воспроизведения--
-	waveHdr[bN].dwBufferLength = WaveFormat.nSamplesPerSec; //--размер аудиопотока--
-	waveHdr[bN].dwFlags = WHDR_INQUEUE;
-	waveHdr[bN].dwLoops = 1;
+	//ZeroMemory(&waveHdr[bN], sizeof(waveHdr[bN])); //--обнуляем WaveHdr--
+
+	//waveHdr[bN].lpData = Buffer; //--присваиваем аудиопоток для воспроизведения--
+	//waveHdr[bN].dwBufferLength = WaveFormat.nSamplesPerSec; //--размер аудиопотока--
+	//waveHdr[bN].dwFlags = WHDR_INQUEUE;
+	//waveHdr[bN].dwLoops = 1;
+	////--Заполнение аудиопотока-------------------------------------------------------------------------------
+
+	//waveOutPrepareHeader(hWaveOut, &waveHdr[bN], sizeof(WAVEHDR));
+
+	//waveOutWrite(hWaveOut, &waveHdr[bN], sizeof(WAVEHDR));
+
+	//NextBufNumber();
+
+	WAVEHDR* whdr = new WAVEHDR();
+	ZeroMemory(&(*whdr), sizeof((*whdr))); //--обнуляем WaveHdr--
+
+	(*whdr).lpData = Buffer; //--присваиваем аудиопоток для воспроизведения--
+	(*whdr).dwBufferLength = WaveFormat.nSamplesPerSec; //--размер аудиопотока--
+	(*whdr).dwFlags = WHDR_INQUEUE;
+	(*whdr).dwLoops = 1;
 	//--Заполнение аудиопотока-------------------------------------------------------------------------------
 
-	waveOutPrepareHeader(hWaveOut, &waveHdr[bN], sizeof(waveHdr));
-	waveOutWrite(hWaveOut, &waveHdr[bN], sizeof(waveHdr));
-	NextBufNumber();
+	waveOutPrepareHeader(hWaveOut, &(*whdr), sizeof(WAVEHDR));
+
+	waveOutWrite(hWaveOut, &(*whdr), sizeof(WAVEHDR));
+
 }
 
 //--Воспроизводим аудиофайл----------------------------------------------------------------------------------
@@ -277,7 +313,7 @@ void PlayBuffer(char *Buffer){
 void PlaySound()
 {
 	//--ВыДеление данных из WAV файла------------------------------------------------------------------------
-
+	
 	if(ParseData()) return;
 
 	//--Открытие аудиоустройства-----------------------------------------------------------------------------
@@ -291,7 +327,9 @@ void PlaySound()
 
 	for(int i=0; i < (size/WaveFormat.nSamplesPerSec); ++i)
 	{
+		while(state[bN]){}
 		Buffer = (sound_buffer + (i * WaveFormat.nSamplesPerSec));
+		Sleep(500);
 		PlayBuffer(Buffer);
 	}
 }
@@ -300,6 +338,159 @@ void PlaySound()
 
 #pragma region WinSocket
 
+//--Server---------------------------------------------------------------------------------------------------
 
+DWORD WINAPI ServerThread(LPVOID pParam)
+{
+	// Инициализируем библиотеку Winsock.
+	WSADATA wsaData;
+	int iResult = WSAStartup( MAKEWORD(2,2), &wsaData );
+	if ( iResult != NO_ERROR )
+		printf("Error at WSAStartup()\n");
+
+	// Получаем данные для инициализации
+	struct addrinfo *res = NULL, hints;
+	memset(&hints,0,sizeof(hints));
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	if ( getaddrinfo("127.0.0.1", "27015", &hints, &res) != 0 )
+	{// обработка ошибки 
+		printf("Can't get initiaalization data!\n");
+		WSACleanup();
+		return 0;
+	}
+	// Инициализируем сокет
+	SOCKET m_socket;
+	m_socket = socket(res->ai_family, res->ai_socktype,res->ai_protocol);
+	if ( m_socket == INVALID_SOCKET ) 
+	{ // Обработка ошибки инициализации
+		printf( "Error at socket(): %ld\n", WSAGetLastError() );
+		freeaddrinfo(res);
+		WSACleanup();
+		return 0;
+	}
+	if ( bind( m_socket, res->ai_addr,res->ai_addrlen) == SOCKET_ERROR ) 
+	{ // Обработка ошибки связывания
+		printf( "bind() failed.\n" );
+		freeaddrinfo(res);
+		closesocket(m_socket);
+		WSACleanup();
+		return 0;
+	}
+
+	// Переводим сокет с пассивное состояние - режим прослушивания.
+	if ( listen( m_socket, 1 ) == SOCKET_ERROR )
+		printf( "Error listening on socket.\n");
+
+	// Принимаем входящее соединение.
+	SOCKET AcceptSocket;
+
+	printf( "Waiting for a client to connect...\n" );
+	while (1) {
+		AcceptSocket = SOCKET_ERROR;
+		while ( AcceptSocket == SOCKET_ERROR ) {
+			AcceptSocket = accept( m_socket, NULL, NULL );
+		}
+		printf( "Client Connected.\n");
+		closesocket(m_socket); 
+		break;
+	}
+	m_socket = AcceptSocket; 
+
+	// Получаем и передаем данные.
+	int bytesSent;
+	int bytesRecv = SOCKET_ERROR;
+	char sendbuf[32] = "Server: Sending Data.";
+	char recvbuf[32] = "";
+
+	/*bytesRecv = recv( m_socket, recvbuf, 32, 0 );
+	printf( "Bytes Recv: %ld\n", bytesRecv );
+
+	bytesSent = send( m_socket, sendbuf, strlen(sendbuf), 0 );
+	printf( "Bytes Sent: %ld\n", bytesSent );*/
+	while(1)
+	{
+		Sleep(0);
+		bytesSent = send( m_socket, number, 44100, 0 );
+	}
+
+	// Корректно завершаем работу
+	// Отключаем сокет
+	shutdown(m_socket,SD_BOTH);
+	// Освобождаем ресурсы, занятые информацией об адресе
+	freeaddrinfo(res);
+	// Закрываем сокет
+	closesocket(m_socket);
+	// Отключаем библиотеку Winsock
+	WSACleanup();
+
+	return 0;
+}
+//--Client---------------------------------------------------------------------------------------------------
+
+DWORD WINAPI ClientThread(LPVOID pParam){
+	// Инициализируем библиотеку Winsock.
+	WSADATA wsaData;
+	int iResult = WSAStartup( MAKEWORD(2,2), &wsaData );
+	if ( iResult != NO_ERROR )
+		printf("Error at WSAStartup()\n");
+
+	// Получаем данные для инициализации
+	struct addrinfo *res = NULL, hints;
+	memset(&hints,0,sizeof(hints));
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	if ( getaddrinfo("127.0.0.1", "27015", &hints, &res) != 0 )
+	{// обработка ошибки 
+		printf("Can't get initiaalization data!\n");
+		WSACleanup();
+		return 0;
+	}
+	// Инициализируем сокет
+	SOCKET m_socket;
+	m_socket = socket(res->ai_family, res->ai_socktype,res->ai_protocol);
+	if ( m_socket == INVALID_SOCKET ) 
+	{ // Обработка ошибки инициализации
+		printf( "Error at socket(): %ld\n", WSAGetLastError() );
+		freeaddrinfo(res);
+		WSACleanup();
+		return 0;
+	}
+	// Подсоединяемся к серверу
+	if ( connect( m_socket, res->ai_addr,res->ai_addrlen) == SOCKET_ERROR) {
+		printf( "Failed to connect.\n" );
+		closesocket(m_socket);
+		freeaddrinfo(res);
+		WSACleanup();
+		return 0;
+	}
+
+	// Передаем и получаем данные.
+	int bytesSent;
+	int bytesRecv = SOCKET_ERROR;
+	char sendbuf[32] = "Client: Sending data.";
+	char recvbuf[44100] = "";
+
+	/*bytesSent = send( m_socket, sendbuf, strlen(sendbuf), 0 );
+	printf( "Bytes Sent: %ld\n", bytesSent );*/
+	while( 1 ) {
+		bytesRecv = recv( m_socket, recvbuf, 44100, 0 );
+		file<<recvbuf;
+		file.flush();
+	}
+
+
+	// Корректно завершаем работу
+	// Отключаем сокет
+	shutdown(m_socket,SD_BOTH);
+	// Освобождаем ресурсы, занятые информацией об адресе
+	freeaddrinfo(res);
+	// Закрываем сокет
+	closesocket(m_socket);
+	// Отключаем библиотеку Winsock
+	WSACleanup();
+
+	return 0;
+}
 
 #pragma endregion
